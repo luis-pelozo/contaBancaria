401ab5030a90285da6b4d491f05cff625a001509
luis-pelozo <pelozoluis52@gmail.com>
2022-09-29 21:12:17 -0300

    inicio fonte

--- contaBancaria/src/visualizacao/EntradaSaida.java
+++ contaBancaria/src/visualizacao/EntradaSaida.java
@@ -1,5 +1,27 @@
 package visualizacao;
 
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
 public class EntradaSaida {
-    
+
+    public static int solicitaOpcao(){
+		String[] opcoes = {"Saque", "Depósito", "Ver Saldo","Imprimir extrato" ,"Sair "};
+		JComboBox<String> menu = new JComboBox<String>(opcoes);
+
+		JOptionPane.showConfirmDialog(null, menu, "Selecione uma opção", JOptionPane.OK_CANCEL_OPTION);
+		return menu.getSelectedIndex();
+	}
+
+
+
+    /*
+    solicitarInformacoesDeposito()
+    solicitarInformacoesSaque()
+    exibirSaldo()
+    exibirDadosDaConta() - exibir nome do titular, tipo e saldo
+    exibirExtratoCompleto()
+    exibirExtratoDeDepositos()
+    exibirExtratoDeSaques()
+ */
 }
